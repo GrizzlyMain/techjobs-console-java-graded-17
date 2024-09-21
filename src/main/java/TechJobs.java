b98f243ffd9edbcb889597f4282e2932d0739baf
GrizzlyMain <ryan.thom.dalton@gmail.com>
2024-09-21 09:45:54 -0500

    Test Commit for WorkFlow 2

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -11,7 +11,7 @@ public class TechJobs {
     static Scanner in = new Scanner(System.in);
 
     public static void main (String[] args) {
-tes
+
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
         columnChoices.put("core competency", "Skill");
